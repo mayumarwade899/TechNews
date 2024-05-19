a6f5f10c103df3a61275dfec0b3a01e161772d03
Mayur Marwade <mayumarwade899@gmail.com>
2024-05-19 20:37:10 +0530

    created userContext & custom hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
-import Pagination from './components/Pagination'
-import Search from './components/Search'
-import Stories from './components/Stories'
+import Pagination from "./components/Pagination";
+import Search from "./components/Search";
+import Stories from "./components/Stories";
+// import { AppContext } from "./components/Context";
+// import { useContext } from "react";
+import { useGlobalContext } from "./components/Context";
 
 const App = () => {
+  const data = useGlobalContext();
   return (
     <>
+      <h1>Welcome {data}</h1>
       <Search />
       <Pagination />
       <Stories />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const Stories = () => {
-  return (
-    <div>Stories</div>
-  )
-}
+  let API = "https://hn.algolia.com/api/v1/search?query=html";
 
-export default Stories
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect = (() => {
+    fetchApiData(API);
+  }, [])
+
+  return (
+    <>
+      <h1>Welcom to tech news!!</h1>
+    </>
+  );
+};
+
+export default Stories;
